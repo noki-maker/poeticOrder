cc4660dbb580f287443f79d00faacd60ff4099a9
efrice <719431749@qq.com>
2025-08-29 18:41:42 +0800

    feat: custom poetic

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -5,26 +5,50 @@ defineOptions({
   name: 'IndexPage',
 })
 
-const poetic = '绿蚁新醅酒，红泥小火炉。晚来天欲雪，能饮一杯无？'
-// const poetic = '两个黄鹂鸣翠柳，一行白鹭上青天。窗含西岭千秋雪，门泊东吴万里船。'
+const poetic = ref('绿蚁新醅酒，红泥小火炉。晚来天欲雪，能饮一杯无？')
+/*
+两个黄鹂鸣翠柳，
+一行白鹭上青天。
+窗含西岭千秋雪，
+门泊东吴万里船。
+*/
+
 
 const passed = ref(false)
 const rows = ref(4)
 const cols = ref(6)
-const initArr = ref(init(rows.value, cols.value, poetic))
+const initArr = ref(init(rows.value, cols.value, poetic.value))
 
 function newGame() {
   passed.value = false
-  initArr.value = init(rows.value, cols.value, poetic)
+  initArr.value = init(rows.value, cols.value, poetic.value)
+}
+
+const visible = ref(false)
+const customPoetic = ref('')
+function customPoeticClick() {
+  visible.value = true
+}
+
+function customPoeticConfirm() {
+  visible.value = false
+  poetic.value = customPoetic.value.replace(/\r\n/g, '').replace(/\r/g, '').replace(/\n+/g, '').trim()
+  rows.value = customPoetic.value.split('\n').length
+  cols.value = Math.floor(customPoetic.value.length / rows.value)
+  console.log({ poeticValue: poetic.value, rows: rows.value, cols: cols.value })
+  newGame()
+}
+function customPoeticCancel() {
+  visible.value = false
 }
 
 function showContent(row: number, col: number) {
-  if(passed.value && !isOrder(initArr.value, poetic ) && row === rows.value) {
+  if(passed.value && !isOrder(initArr.value, poetic.value) && row === rows.value) {
     if(col === cols.value - 2 || col === cols.value - 1) {
-      return poetic[(row - 1) * cols.value + col - 1]
+      return poetic.value[(row - 1) * cols.value + col - 1]
     }
   }
-  return initArr.value[(row - 1) * cols.value + (col - 1)].id === rows.value * cols.value - 1 && !passed.value ? '' : poetic[initArr.value[(row - 1) * cols.value + (col - 1)].id]
+  return initArr.value[(row - 1) * cols.value + (col - 1)].id === rows.value * cols.value - 1 && !passed.value ? '' : poetic.value[initArr.value[(row - 1) * cols.value + (col - 1)].id]
 }
 
 function handleClick(id: number) {
@@ -46,7 +70,7 @@ function handleClick(id: number) {
 
     if (res && res.length > 0) {
       initArr.value = res
-      if (isOK(res, poetic))
+      if (isOK(res, poetic.value))
         passed.value = true
     }
   }
@@ -71,7 +95,7 @@ onMounted(() => {
     }
     if (res && res.length > 0) {
       initArr.value = res
-      if (isOK(res, poetic))
+      if (isOK(res, poetic.value))
         passed.value = true
     }
   })
@@ -86,6 +110,12 @@ onMounted(() => {
         @click="newGame"
       >
         重 来
+      </button>
+      <button
+        class="m-3 text-sm btn"
+        @click="customPoeticClick"
+      >
+        自定义
       </button>
     </div>
 
@@ -111,6 +141,18 @@ onMounted(() => {
           >
           {{ showContent(row, col) }}
         </button>
+      </div>
+    </div>
+
+    <div v-show="visible" class="fixed top-0 left-0 right-0 bottom-0 bg-black bg-opacity-50 flex items-center justify-center">
+      <div class="w200 border rounded-xl p-4 bg-white">
+        <p class="pb-2">自定义诗句(记得换行)</p>
+        <textarea v-model="customPoetic" rows="10" cols="88" class="border p-2 outline-none"></textarea>
+        <div class="flex">
+          <div class="flex-auto"></div>
+          <button type="button" class="m-3 text-sm btn bg-white text-gray-800 border hover:bg-white" @click="customPoeticCancel">取消</button>
+          <button type="button" class="m-3 text-sm btn" @click="customPoeticConfirm">确定</button>
+        </div>
       </div>
     </div>
 
